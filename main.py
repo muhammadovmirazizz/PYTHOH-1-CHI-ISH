1ea9f3b606e8491041de08d95fec613dca7727f1
muhammadovmirazizz <muhammadovmiraziz161@gmail.com>
2024-07-12 08:48:15 +0500

    7-DARS

---
+++ main.py
@@ -0,0 +1,47 @@
+# Tuple o'zgarmas list
+
+# def funksiya (*args):
+#     ismlar = list(args)
+#     ismlar.append("Miraziz")
+    
+#     return ismlar    
+# print(funksiya ("Jhon","Steve"))
+# n = 0
+# for ism in funksiya("Jhon", "Steve","Bob"):
+#     n += 1
+    
+#     print (n, ism)
+
+# def funksiya (*args,**kwargs):
+#     ismlar = list(args)
+#     ismlar.append("Miraziz")
+    
+#     return kwargs
+
+# Mualif = input("Mualifini kiriting >>>")
+# Kitobnarxi =input(20)
+# kitobhaqida =input("")
+
+
+# user = funksiya(Mualifi=Mualif, Kitobnarxi=Kitobnarxi)
+# print(user)
+
+# Kitoblar = {
+#     "Harry poter":{
+#         "Author":"	J. K. Rowling",
+#         "Price":"20$",
+#         "Description":"Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter",
+#         "Year":"1997"
+#     }
+# }
+# if
+ 
+# def AddKitob(BookName, Author, Price, Description, Year):
+#     Kitoblar[BookName] = {
+#         "Author":Author,
+#         "Price":Price,
+#         "Description":Description,
+#         "Year":Year,
+#     }
+# print (Kitoblar)
+
